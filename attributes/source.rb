954901754e5e06c6db764f4f177edc9a4b7b2788
Yann <me.show@gmail.com>
2015-04-08 10:23:07 +0200

    update checksum

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,2 +1,2 @@
 default['imagemagick']['source']['url'] = "http://www.imagemagick.org/download/ImageMagick-6.9.1-1.tar.gz"
-default['imagemagick']['source']['checksum'] = "5cd7c5dbb6ab9aaced5c52e9b4bd8ee11bb8041ab95baaf6388a4af4fe96df93"
+default['imagemagick']['source']['checksum'] = "1aa629ab63ee2ba1fd30f12de129d69f4d4c419e4394a2fc263094a49ffb0e8c"
