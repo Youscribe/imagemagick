01f26ab3ec7a444e7de3e0e24657751a1216ef7f
Yann <me.show@gmail.com>
2015-04-08 10:18:57 +0200

    update download url

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,2 +1,2 @@
-default['imagemagick']['source']['url'] = "http://www.imagemagick.org/download/ImageMagick-6.9.1-0.tar.gz"
+default['imagemagick']['source']['url'] = "http://www.imagemagick.org/download/ImageMagick-6.9.1-1.tar.gz"
 default['imagemagick']['source']['checksum'] = "5cd7c5dbb6ab9aaced5c52e9b4bd8ee11bb8041ab95baaf6388a4af4fe96df93"
